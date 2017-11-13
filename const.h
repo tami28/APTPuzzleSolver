4fe054607a793fcc8a45c101319231ae5ac22fcb
yoav1412 <Yoav.BenEphraim@forter.com>
2017-11-13 16:35:39 +0200

    added int values to Edged, to use them as indices in the Piece array of edges

--- const.h
+++ const.h
@@ -3,4 +3,4 @@
 
 extern int numPieces;
 enum Constraints { FEMALE = -1, STRAIGHT = 0, MALE = 1, NONE = 9};
-enum Edge {RIGHT, LEFT, UP, DOWN};
+enum Edge {LEFT = 0, TOP = 1, RIGHT = 2, BOTTOM = 3};
