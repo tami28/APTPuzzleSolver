f3b046c71aee90d1b38496975e7a164b195e2ddf
tami28 <tami28lavi@gmail.com>
2017-12-17 15:12:16 +0200

    no std

--- ConstraintsTable.h
+++ ConstraintsTable.h
@@ -22,5 +22,5 @@ public:
     ConstraintsTable();
     void insertPiece(PuzzlePiece& p);
     set<IDandRotation> getIDsFittingConstraints(int consts[4]);
-    std::unordered_map< int, set<IDandRotation> > _table;
+    unordered_map< int, set<IDandRotation> > _table;
 };
