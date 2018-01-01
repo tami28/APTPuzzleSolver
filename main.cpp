5104ca8aef36a9eee731c489c461f19f36cd9e34
yoav <yoav1412@gmail.com>
2018-01-01 09:05:39 +0200

    main print msgs to defines..

--- main.cpp
+++ main.cpp
@@ -9,6 +9,13 @@
 #include <chrono>
 #include "const.h"
 
+#define NO_INPUT_FILE_ERR_MSG "Error, no input file. Run COMMAND inputFilePath or COMMAND inputFilePath outputFilePath for specific output file location"
+#define FLAG_USED_ONCE_ERR_MSG "Each flag should be used once."
+#define WRONG_FLAG_USAGE_ERR_MSG "Wrong usage: should call flag -thread <d> with <d> number of wanted threads"
+#define TOO_MANY_ARGS_ERR_MSG "Too many args, unused parameter: "
+#define ROTATE_FLAG "-rotate"
+#define THREADS_FLAG "-threads"
+
 std::string outFilePath;
 bool withRotations = false;
 
@@ -23,33 +30,32 @@ bool isCharPntInt(char* argv){
 
 
 int main(int argc, char** argv){
-
     bool hasInputFile = false, hasOutputFile = false, hasThreads = false;
     std::string inFilePath;
     outFilePath = DEFAULT_OUTPUT_FILE;
     int numThreads = DEFAULT_NUM_THREADS;
     if (argc <2){
-        std::cout<<"Error, no input file. Run COMMAND inputFilePath or COMMAND inputFilePath outputFilePath for specific output file location";
+        std::cout<<NO_INPUT_FILE_ERR_MSG;
         return 1;
     }
     for(int i =1; i<argc; i++){
-        if ((strcmp(argv[i], "-rotate") == 0)){
+        if ((strcmp(argv[i], ROTATE_FLAG) == 0)){
             if (withRotations){
-                std::cout<<"Each flag should be used once."<<std::endl;
+                std::cout<<FLAG_USED_ONCE_ERR_MSG<<std::endl;
                 return 1;
             }
             withRotations = true;
             continue;
         }
-        if ((strcmp(argv[i], "-threads") == 0)){
+        if ((strcmp(argv[i], THREADS_FLAG) == 0)){
             if (hasThreads){
-                std::cout<<"Each flag should be used once."<<std::endl;
+                std::cout<<FLAG_USED_ONCE_ERR_MSG<<std::endl;
                 return 1;
             }
             hasThreads = true;
             i++;
             if (i >= argc || !(isCharPntInt(argv[i]))){
-                std::cout<<"Wrong usage: should call flag -thread <d> with <d> number of wanted threads"<<std::endl;
+                std::cout<<WRONG_FLAG_USAGE_ERR_MSG<<std::endl;
                 return 1;
             }
             numThreads = atoi(argv[i]);
@@ -65,11 +71,9 @@ int main(int argc, char** argv){
             hasOutputFile = true;
             continue;
         }
-        std::cout <<"To many args, unused parameter: "<< argv[i] << std::endl;
+        std::cout <<TOO_MANY_ARGS_ERR_MSG<< argv[i] << std::endl;
         return 1;
     }
-
-
     // Try to solve:
     Solver solver(inFilePath, numThreads);
     // Check for Errors before continuing to solve:
